80c24476c08910e7b5d2d857082e78dee14f4f93
Morgana Galamba <mbfg2@cin.ufpe.br>
2021-04-18 21:47:37 -0300

    change th preview mode

--- freve.playground/Contents.swift
+++ freve.playground/Contents.swift
@@ -5,11 +5,14 @@ import GameplayKit
 import Foundation
 import PlaygroundSupport
  
-let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 640, height: 480))
-var scene = Menu(fileNamed: "Menu")
-sceneView.presentScene(scene)
+// Load the SKScene from 'GameScene.sks'
+ let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 640, height: 480))
+ if let scene = Menu(fileNamed: "Menu") {
+     // Set the scale mode to scale to fit the window
+     scene.scaleMode = .aspectFill
 
+     // Present the scene
+     sceneView.presentScene(scene)
+ }
 
-PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
-
-
+ PlaygroundSupport.PlaygroundPage.current.liveView = sceneView
