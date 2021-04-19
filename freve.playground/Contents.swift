162ff1a6d3f17bb02e3dc3192a1a5c329082e281
Morgana Galamba <mbfg2@cin.ufpe.br>
2021-04-18 21:45:45 -0300

    add texture to the scene

--- freve.playground/Contents.swift
+++ freve.playground/Contents.swift
@@ -7,7 +7,6 @@ import PlaygroundSupport
  
 let sceneView = SKView(frame: CGRect(x:0 , y:0, width: 640, height: 480))
 var scene = Menu(fileNamed: "Menu")
-//scene.scaleMode = .aspectFill
 sceneView.presentScene(scene)
 
 
